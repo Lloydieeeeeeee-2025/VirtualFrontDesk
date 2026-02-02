aee3ee656b2da59ca21642f88022fba3946310d3
lloydieeeeeeee-2025 <lloyddeleon@thelewiscollege.edu.ph>
2026-02-02 10:03:26 +0800

    update database connector

--- backend/dbconnector/db.py
+++ backend/dbconnector/db.py
@@ -1,12 +1,22 @@
+import os
 import mysql.connector
 
 def tlcchatmate():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST", "db"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", "password"), 
+        database=os.getenv("DB_NAME", "tlcchatmate"),
+        port=int(os.getenv("DB_PORT", "3306")),
+    )
+
+"""def tlcchatmate():
     return mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
         database="tlcchatmate"
-    )
+    )"""
 
 """
 def dbconnection():
